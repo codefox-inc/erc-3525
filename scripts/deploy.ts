ed224dfc0bfc3a4455a78fa62172f865b41e9a45
Shiho Nagabuchi <pc220557@PC220557.local>
2022-11-02 22:02:00 +0900

    add @nomiclabs/hardhat-ethers

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,6 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 
 async function main() {
